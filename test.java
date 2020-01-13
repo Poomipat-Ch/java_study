20a5161ac91167fb2ccb1afc2a4f22aa709980d4
poomipat01 <poomipat002@gmail.com>
2020-01-13 20:08:24 +0700

    update

--- test.java
+++ test.java
@@ -1,9 +1,5 @@
-class test{
-    
-
-    public static void main(String[] args) {
+public class test{
+        public static void main(String[] args) {
         System.out.print("Test");
     }
-
-
 }
